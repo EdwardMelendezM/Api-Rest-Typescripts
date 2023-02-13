475e216724a0382a4c94f84243d2d5cc8869ab0f
EdwardMelendezM <106676335+EdwardMelendezM@users.noreply.github.com>
2023-02-12 21:37:38 -0500

    Agregando coneccion a mongodb

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,16 @@
 import express from "express";
 import "dotenv/config";
 import cors from "cors";
+import { router } from "./routes";
+import db from "./config/mongo";
 const PORT = process.env.PORT || 3001;
 const app = express();
+
 app.use(cors());
+app.use(router);
+db().then(() => console.log("Conection ready"));
 app.listen(PORT, () => {
   console.log(`Server on port ${PORT}`);
 });
+
 export { app };
---
+++ src/routes/item.ts
@@ -0,0 +1,7 @@
+import { Request, Response, Router } from "express";
+
+const router = Router();
+router.get("/", (req: Request, res: Response) => {
+  res.send({ data: "AQUI VAN LOS MODELOS" });
+});
+export { router };
